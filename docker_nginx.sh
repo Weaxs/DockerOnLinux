73d19e525da5d8cfa2d3e4a75c76011c6e5a9cca
Weaxs <459312872@qq.com>
2020-09-27 00:39:45 +0800

    Update docker_nginx.sh

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -7,3 +7,13 @@ docker pull nginx
 # search nginx version
 docker search nginx
 
+docker run --name my_nginx -p 80:80 -p 443:443 -d nginx
+
+docker container ls
+docker container ls -a
+
+docker ps
+
+docker start my_nginx
+docker stop my_nginx
+docker rm my_nginx
