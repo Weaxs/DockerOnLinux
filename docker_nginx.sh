cd6d552152a78f5a5c789710f911b827ec49ccd9
Weaxs <459312872@qq.com>
2020-09-27 14:03:37 +0800

    Update docker_nginx.sh

--- docker_nginx.sh
+++ docker_nginx.sh
@@ -7,7 +7,21 @@ docker pull nginx
 # search nginx version
 docker search nginx
 
+# create docker container
 docker run --name my_nginx -p 80:80 -p 443:443 -d nginx
+# 前面是机器上的目录(可变)，后面目录是docker容器上的目录
+docker run \
+  --name my_nginx \
+  -d -p 80:80 \
+  -v /usr/docker/my_nginx/html:/usr/share/nginx/html \
+  -v /etc/docker/my_nginx/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v /etc/docker/my_nginx/conf.d:/etc/nginx/conf.d \
+  nginx
+
+# enter into container
+docker container exec -it my_nginx /bin/bash
+# view container log
+docker logs -f -t --tail 20 my_nginx
 
 docker container ls
 docker container ls -a
