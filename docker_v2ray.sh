6c856f03089d9c642a4351aad2ce7a4884a42f5f
Weaxs <459312872@qq.com>
2020-09-29 02:29:30 +0800

    Update docker_v2ray.sh

--- docker_v2ray.sh
+++ docker_v2ray.sh
@@ -5,6 +5,10 @@ docker pull v2fly/v2fly-core
 
 docker run --name v2ray -v /etc/v2ray/config.json:/etc/v2ray/config.json:ro v2fly/v2fly-core
 
-docker run --name v2ray \
+docker run -itd \
+--restart always \
+--name v2ray \
 -v /etc/v2ray/config.json:/etc/v2ray/config.json:ro \
+-v /var/log/v2ray:/var/log/v2ray \
 v2fly/v2fly-core
+
