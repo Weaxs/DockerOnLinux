320a89a3c9bf4fb4e368f19e527c1dbd93e34880
Weaxs <459312872@qq.com>
2020-09-30 01:20:53 +0800

    Create docker_mysql.sh

---
+++ docker_mysql.sh
@@ -0,0 +1,16 @@
+# https://hub.docker.com/_/mysql
+
+
+
+docker run --name mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+
+docker run --name mysql-test -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_USER=user -e MYSQL_PASSWORD=123456 -d mysql
+
+docker run --name some-mysql -e MYSQL_ROOT_PASSWORD=my-secret-pw -d mysql:tag --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
+
+
+
+docker exec -it mysql-test bash
+
+docker logs mysql-test
+
