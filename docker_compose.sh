fcb4ffced2dd9eec64b4e07f2ce2c6986dfd7b03
Weaxs <459312872@qq.com>
2020-09-30 14:23:40 +0800

    Create docker_compose.sh

---
+++ docker_compose.sh
@@ -0,0 +1,8 @@
+# https://docs.docker.com/compose/
+# https://github.com/docker/compose
+
+# 安装docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
