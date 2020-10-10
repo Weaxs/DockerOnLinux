0a757f51e36fd3e63bc4374fc477294dcaca37f8
Weaxs <459312872@qq.com>
2020-10-10 16:41:36 +0800

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -6,3 +6,10 @@ curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compo
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
+# 查看 docker-compose.yml 或 docker-compose.yaml 是否合法
+docker-compose config
+
+# 启动
+docker-compose up
+# 后台启动
+docker-compose up -d
